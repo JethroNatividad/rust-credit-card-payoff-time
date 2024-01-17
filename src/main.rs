74eae5dfb9cb1108b08fec175fc6fbf81bd5f95d
JethroNatividad <natividad.jet@gmail.com>
2024-01-17 22:42:00 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // A program that helps you determine how many months it will take to pay off credit card balance.
+// Inputs: balance, daily_rate or APR, monthly_payment.
+// Process: (-1/30) * (log(1 + ((balance / monthly_payment)*(1 - pow(1 + daily_rate, 30))) ) / log(1 + daily_rate))
+// output: It will take you {} months to pay off this card
 
 fn main() {
     println!("Hello, world!");
