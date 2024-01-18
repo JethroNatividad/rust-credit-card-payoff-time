c54c752bfc7a5dff19519767cd90493576909aa6
JethroNatividad <natividad.jet@gmail.com>
2024-01-18 22:32:40 +0800

    return i64

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::io::Write;
 // Process: (-1/30) * (log(1 + ((balance / monthly_payment)*(1 - pow(1 + (apbr / 365), 30))) ) / log(1 + (apbr / 365)))
 // output: It will take you {} months to pay off this card
 
-fn calculate_credit_card_payoff_time(balance: f64, apbr: f64, monthly_payment: f64) -> f64 {
+fn calculate_credit_card_payoff_time(balance: f64, apbr: f64, monthly_payment: f64) -> i64 {
     if balance < 1.0 {
-        return 0.0;
+        return 0;
     }
     // calculate monthly payment
     let daily_rate: f64 = (apbr / 100.0) / 365.0;
@@ -16,7 +16,7 @@ fn calculate_credit_card_payoff_time(balance: f64, apbr: f64, monthly_payment: f
         * (1.0 + (balance / monthly_payment * (1.0 - (1.0 + daily_rate).powf(30.0)))).log10()
         / (1.0 + daily_rate).log10();
     // round up
-    res.ceil()
+    res.ceil() as i64
 }
 #[cfg(test)]
 mod tests {
@@ -24,9 +24,9 @@ mod tests {
 
     #[test]
     fn test_credit_card_payoff_time() {
-        assert_eq!(calculate_credit_card_payoff_time(5000.0, 12.0, 100.0), 70.0);
-        assert_eq!(calculate_credit_card_payoff_time(100.0, 18.0, 10.0), 11.0);
-        assert_eq!(calculate_credit_card_payoff_time(0.0, 15.0, 50.0), 0.0);
+        assert_eq!(calculate_credit_card_payoff_time(5000.0, 12.0, 100.0), 70);
+        assert_eq!(calculate_credit_card_payoff_time(100.0, 18.0, 10.0), 11);
+        assert_eq!(calculate_credit_card_payoff_time(0.0, 15.0, 50.0), 0);
     }
 }
 
@@ -49,9 +49,13 @@ fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
 
 fn main() {
     // get input_balance, "What is your balance? "
+    let input_balance: f64 = get_input("What is your balance? ");
     // get apbr, "What is the APR on the card (as a percent)? "
+    let apbr: f64 = get_input("What is the APR on the card (as a percent)? ");
     // get monthly_payment, "What is the monthly payment you can make? "
+    let monthly_payment: f64 = get_input("What is the monthly payment you can make? ");
     // calculate months
+    let months: i64
 
     // print, "It will take you 70 month/s to pay off this card"
 }
